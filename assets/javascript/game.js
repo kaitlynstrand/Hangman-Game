cd2aea000628a2faa338f5d3b5c4e6480d270775
Kaitlyn <kaitlyn.strand@gmail.com>
2017-09-21 18:29:01 -0500

    soclosegoodproject

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wordList = ["bunny", "hamster", "gerbil", "ferret", "parrot", "kitty", "puppy", "horse", "goldfish", "snake", "iguana"]
+var wordList = ["cat", "tiger", "lion", "leopard", "cheetah", "jaguar", "cougar", "wildcat", "panther", "lynx", "ocelot"]
 
 var letterGuess = " "
 var wordListElem = document.getElementById("word-list")
@@ -6,22 +6,38 @@ var letterGuessElem = document.getElementById("letter-guess")
 var livesLeftElem = document.getElementById("lives-left")
 var winsGameElem = document.getElementById("wins")
 
-
 var word = ""
-var lives = 5
+var lives
 var answerArray = []
 var isGameStarted = false
+var wins = 0
 
 //var array alphabet =  with letters of the alphabet 
 //alphabet.index(event) == false
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+// tried this but couldn't make it work!!
+// var audioElem = document.createElement("tomjones");
+// //audioElem.setAttribute("src", ".assets/tomjones.mp3");
+//     function(tomjones) {
+//       if (event.keyCode ==32) {
+// 	audioElem.play();
+//       }
+//      }
+
+//  audioElem.setAttribute("src", ".assets/tomjones.mp3");
+
+
 document.onkeypress = function(event) {
 	var userGuess = event.key
-	letterGuess = letterGuess + userGuess
+	letterGuess += userGuess + " "
 	console.log(letterGuess)
-
-	if (isGameStarted) {
+	if (event.keyCode == 32) {
+		//space bar hit start a new game
+		initialNewGame()
+		userGuess = ""
+	}
+	else if (isGameStarted) { 
 		//check if the game is over
 		if (lives <= 0) {
 			//figure out how to print to the screen that the game is done
@@ -33,9 +49,13 @@ document.onkeypress = function(event) {
 				lives -= 1
 			}
 		}
+		if (isGameWon()) {
+			initialNewGame()
+			userGuess = ""
+		}
 	}
-
 	else {
+		initialNewGame()
 		word = selectWord()
 		for (var i = 0; i < word.length; i++) {
 			answerArray[i] = "_"
@@ -45,13 +65,30 @@ document.onkeypress = function(event) {
 	wordListElem.textContent = answerArray.join(" ")
 	letterGuessElem.textContent = letterGuess 
 	livesLeftElem.textContent = lives
+	winsGameElem.textContent = wins
 };
 
+function isGameWon() {
+	var lettersInAnswerArray = 0
+	for (var i = 0; i < answerArray.length; i++) {
+		if (alphabet.includes(answerArray[i])) {
+			lettersInAnswerArray += 1
+		}
+	}
+	//this is what happens when you win
+	if (word.length === lettersInAnswerArray) {
+		wins += 1
+		return true
+	}
+	return false
+}
+
 function initialNewGame() {
-	lives = 5
+	lives = 15
 	word = ""
 	isGameStarted = false
 	answerArray = []
+	letterGuess = ""
 }
 
 function selectWord() {
